12a77afdbed2276db98087f40ffe41adcba9aa1a
Georg Krause <mail@georg-krause.net>
2022-12-09 13:20:56 +0100

    feat: Download binaries before running build

---
+++ scripts/download-beibootctl.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+download_url=$(curl https://api.github.com/repos/Getdeck/beiboot/releases/latest | jq -r ".assets[0].browser_download_url")
+curl -o download.zip -L $download_url
+unzip -d /tmp/ -o download.zip
+mkdir -p src-tauri/binaries
+mv -f /tmp/beibootctl src-tauri/binaries/beibootctl-x86_64-unknown-linux-gnu
+rm -rf download.zip
