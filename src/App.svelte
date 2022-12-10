98d2f20e79008564ca84dab7aa56d039b5959440
Georg Krause <mail@georg-krause.net>
2022-12-10 10:44:35 +0100

    Add svelte prettifier plugin

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,51 @@
 <script>
-import { getVersion, createCluster } from './beibootctl'
+  import { getVersion, createCluster } from "./beibootctl";
 
-let promise = getVersion();
-let clusterName;
-let handlerPromise;
-let promiseRunning = false;
-let error = "";
+  let promise = getVersion();
+  let clusterName;
+  let handlerPromise;
+  let promiseRunning = false;
+  let error = "";
 
-function createClusterHandler() {
-  error = "";
-  promiseRunning = true;
-  console.log(clusterName)
-  if(clusterName) {
-    handlerPromise = createCluster(clusterName).then( (output) => { 
-      promiseRunning = false; 
-      console.log(output);
-      if (output.code !== 0) {
-        error = output.stderr;
-      }
-    });
+  function createClusterHandler() {
+    error = "";
+    promiseRunning = true;
+    console.log(clusterName);
+    if (clusterName) {
+      handlerPromise = createCluster(clusterName).then((output) => {
+        promiseRunning = false;
+        console.log(output);
+        if (output.code !== 0) {
+          error = output.stderr;
+        }
+      });
+    }
   }
-}</script>
+</script>
 
 <div id="app">
-<img class="logo-light" src="beiboot-logo-light.png">
-<img class="logo-dark" src="beiboot-logo-dark.svg">
-
+  <img class="logo-light" src="beiboot-logo-light.png" alt="Getdeck Beiboot" />
+  <img class="logo-dark" src="beiboot-logo-dark.svg" alt="Getdeck Beiboot" />
 
   {#await promise}
     <p>Loading…</p>
-    {:then version}
+  {:then version}
     <p>{version}</p>
   {/await}
 
-<input bind:value={clusterName}>
+  <input bind:value={clusterName} />
 
-<button on:click={createClusterHandler} disabled={promiseRunning}>Create Cluster</button>
-<button on:click={createClusterHandler}>Delete Cluster</button>
-{#await handlerPromise}
-  <p>Cluster gets created</p>
+  <button on:click={createClusterHandler} disabled={promiseRunning}
+    >Create Cluster</button
+  >
+  <button on:click={createClusterHandler}>Delete Cluster</button>
+  {#await handlerPromise}
+    <p>Cluster gets created</p>
   {:then clusterOutput}
-    {#if error }
+    {#if error}
       <p>{error}</p>
     {:else}
       <p>Cluster created successfully</p>
     {/if}
-
-{/await}
-
+  {/await}
 </div>
-
