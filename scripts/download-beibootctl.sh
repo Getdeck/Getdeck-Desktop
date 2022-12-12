a5ec067638e6ab5fca3daedd5f376d8ddd7e8349
Georg Krause <mail@georg-krause.net>
2022-12-12 09:31:07 +0100

    fix: Adjust binary permissions after download

--- scripts/download-beibootctl.sh
+++ scripts/download-beibootctl.sh
@@ -5,4 +5,5 @@ curl -o download.zip -L $download_url
 unzip -d /tmp/ -o download.zip
 mkdir -p src-tauri/binaries
 mv -f /tmp/beibootctl src-tauri/binaries/beibootctl-x86_64-unknown-linux-gnu
+chmod +x src-tauri/binaries/beibootctl-x86_64-known-linux-gnu
 rm -rf download.zip
