621fb8d23b595e2bbd068207ec9491b8286657c1
Robert Stein <robert@blueshoe.de>
2023-01-04 17:54:46 +0100

    chore: migrate to vue3
    Signed-off-by: Robert Stein <robert@blueshoe.de>

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+        <v-card>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        location="left"
+        permanent
+      >
+          <v-list
+          :items="items"
+        ></v-list>
+        <template v-slot:append>
+            <v-list>
+              <v-list-item title="Settings" value="settings" />
+            </v-list>
+        </template>
+      </v-navigation-drawer>
+    </v-card>
+
+    <default-view />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import DefaultView from './View.vue'
+
+  const drawer = ref(true);
+  const items = ref([
+    { title: 'Clusters', icon: 'mdi-home', value: '/' },
+    { title: 'Inventory', icon: 'mdi-account', value: '/' },
+    { title: 'Local Containers', icon: 'mdi-email', value: '/' },
+  ])
+
+</script>
