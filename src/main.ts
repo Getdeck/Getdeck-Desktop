ba6d83fc2ec098389b7348278433c8f194840dbf
Tobias Frölich <tobias@blueshoe.de>
2023-06-14 15:59:08 +0200

    chore: bump sentry

--- src/main.ts
+++ src/main.ts
@@ -25,23 +25,29 @@ registerPlugins(app)
 OpenAPI.BASE = "https://api.getdeck.dev"
 
 
-Sentry.init({
-  app,
-  dsn: "https://dd838e60a73d4902aa336ca38093e530@o146863.ingest.sentry.io/4505356797018112",
-  integrations: [
-    new Sentry.BrowserTracing({
-      tracePropagationTargets: ["localhost", "tauri.localhost"],
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 0.1,
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+if (process.env.NODE_ENV !== 'dev') {
+  Sentry.init({
+    app,
+    dsn: "https://dd838e60a73d4902aa336ca38093e530@o146863.ingest.sentry.io/4505356797018112",
+      integrations: [
+      new Sentry.BrowserTracing({
+        tracePropagationTargets: ["localhost", "tauri.localhost"],
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      }),
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 0.1,
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 app.mount('#app')
-document.addEventListener('contextmenu', event => event.preventDefault());
+
+if (process.env.NODE_ENV !== 'dev' && process.env.NODE_ENV !== 'debug') {
+  document.addEventListener('contextmenu', event => event.preventDefault());
+}
+
 initKeycloak();
