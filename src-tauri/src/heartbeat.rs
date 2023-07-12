26a0f2140f0ac49514d3140e81749069cdaf7ecc
Tobias Frölich <tobias@blueshoe.de>
2023-07-12 22:42:52 +0200

    test: cover heartbeat

--- src-tauri/src/heartbeat.rs
+++ src-tauri/src/heartbeat.rs
@@ -1,8 +1,8 @@
 use beiboot_desktop::connection::ConnectError;
 
-pub async fn establish_heartbeat_connection(cluster_id: &str, token: &str) -> Result<String, ConnectError> {
+pub async fn establish_heartbeat_connection(cluster_id: &str, token: &str, getdeck_url: &str) -> Result<String, ConnectError> {
     let resp = reqwest::Client::new()
-        .post(format!("https://api.getdeck.dev/clusters/{}/heartbeat", cluster_id).as_str())
+        .post(format!("{}/clusters/{}/heartbeat", getdeck_url, cluster_id).as_str())
         .header(reqwest::header::AUTHORIZATION, format!("Bearer {}", token))
         .send()
         .await
@@ -10,3 +10,22 @@ pub async fn establish_heartbeat_connection(cluster_id: &str, token: &str) -> Re
     Ok(format!("Heartbeat status: {}", resp.status()))
 }
 
+#[cfg(test)]
+mod tests {
+    use super::establish_heartbeat_connection;
+
+    #[tokio::test]
+    async fn test_establish_heartbeat_connection_success() {
+        let mut server = mockito::Server::new();
+        let url = server.url();
+
+        let mock = server.mock("POST", "/clusters/123/heartbeat")
+        .with_status(200)
+        .create();
+
+        let result = establish_heartbeat_connection("123", "token", &url).await;
+        mock.assert();
+        assert_eq!("Heartbeat status: 200 OK", result.unwrap())
+    }
+
+}
